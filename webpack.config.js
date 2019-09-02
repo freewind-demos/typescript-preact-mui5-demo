bec5733bf47e6646ffb9f3bcef4f5ac002bc777a
Peng Li <peng.li@compass.com>
2019-09-02 15:30:05 +0800

    restruct directories; update to latest

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     mode: 'development',
-    entry: './entry.tsx',
+    entry: './src/entry.tsx',
     devtool: "inline-source-map",
     output: {
         path: path.resolve(__dirname, 'dist'),
